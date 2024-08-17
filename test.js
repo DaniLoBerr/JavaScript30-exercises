da47d3da2df44df7229546d64534ffef3a3e71e9
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-08-17 19:29:26 +0200

    Complete "07-Array Cardio Day 2" exercises

--- test.js
+++ test.js
@@ -1,20 +1,12 @@
-const inventors = [
-{ first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-{ first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-{ first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-{ first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-{ first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-{ first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-{ first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-{ first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-{ first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-{ first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-{ first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-{ first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
 ];
 
-// Array.prototype.reduce()
-// 4. How many years did all the inventors live all together?
-console.log(
-  inventors.sort((inventor1, inventor2) => inventor1.year > inventor2.year ? -1 : +1)
-);
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+
